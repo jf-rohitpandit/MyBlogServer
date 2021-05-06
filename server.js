bd4d6ab038318a4c0b5aa465e84f95a4725bb370
Rohit Pandit <rohit0311@outlook.com>
2021-05-06 07:28:00 +0530

    adds server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const morgan = require('morgan');
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = 5000;
+
+app.get('/', (req, res) => {
+	res.json({ message: 'Hello world' });
+});
+
+app.listen(PORT, () => {
+	console.log('server live at port:', PORT);
+});
