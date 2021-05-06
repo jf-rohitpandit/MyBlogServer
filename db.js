1eed77c8e20c5f5b0065574575ca451fcebe1048
Rohit Pandit <rohit0311@outlook.com>
2021-05-06 07:49:44 +0530

    adds db.js

---
+++ db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(
+	'mongodb+srv://rohit:rohit@cluster0.6egng.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+	{ useNewUrlParser: true, useUnifiedTopology: true }
+);
+
+const db = mongoose.connection;
+
+db.on('error', (error) => console.log(error));
+db.once('open', () => console.log('Database connected'));
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const morgan = require('morgan');
+require('./db');
 
 app.use(cors());
 app.use(morgan('dev'));
