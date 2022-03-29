82f4490698519c18bc555f0f24b0fc4f0405825f
Rohit Pandit <rohit0311@outlook.com>
2022-03-29 17:11:11 +0530

    minor change

--- models/commentModel.js
+++ models/commentModel.js
@@ -1,22 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 const commentSchema = mongoose.Schema({
-	user: {
-		type: mongoose.Types.ObjectId,
-		required: true,
-	},
-	text: {
-		type: String,
-		required: true,
-	},
-});
+  user: {
+    type: mongoose.Types.ObjectId,
+    required: true,
+    ref: 'User',
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+})
 
 const commentsSchema = mongoose.Schema({
-	postId: {
-		type: mongoose.Types.ObjectId,
-		required: true,
-	},
-	commentList: [{ type: commentSchema }],
-});
+  postId: {
+    type: mongoose.Types.ObjectId,
+    required: true,
+  },
+  commentList: [{ type: commentSchema }],
+})
 
-module.exports = mongoose.model('comments', commentsSchema);
+module.exports = mongoose.model('comments', commentsSchema)
--- routes/blog.js
+++ routes/blog.js
@@ -1,178 +1,178 @@
-const epxress = require('express');
-const router = epxress.Router();
-const jwt = require('jsonwebtoken');
-const Blog = require('../models/blogModel');
-const Likes = require('../models/likeModel');
-const Comments = require('../models/commentModel');
-const verifyUser = require('../middlewares/authMiddleware');
-const ObjectId = require('mongoose').Types.ObjectId;
+const epxress = require('express')
+const router = epxress.Router()
+const jwt = require('jsonwebtoken')
+const Blog = require('../models/blogModel')
+const Likes = require('../models/likeModel')
+const Comments = require('../models/commentModel')
+const verifyUser = require('../middlewares/authMiddleware')
+const ObjectId = require('mongoose').Types.ObjectId
 
 router.post('/', verifyUser, async (req, res) => {
-	try {
-		const { title, content } = req.body;
-		if (!title || !content) {
-			res.status(400).json({ message: 'Enter all the required details' });
-			return;
-		}
+  try {
+    const { title, content } = req.body
+    if (!title || !content) {
+      res.status(400).json({ message: 'Enter all the required details' })
+      return
+    }
 
-		const blog = new Blog({ title, content, writer: req.userId });
-		await blog.save();
+    const blog = new Blog({ title, content, writer: req.userId })
+    await blog.save()
 
-		const like = new Likes({ postId: blog._id });
-		await like.save();
+    const like = new Likes({ postId: blog._id })
+    await like.save()
 
-		res.status(201).json({ message: 'posted successfully' });
-	} catch (error) {
-		console.log(error);
-		res
-			.status(500)
-			.json({ message: 'Internal error occured, try after some time' });
-	}
-});
+    res.status(201).json({ message: 'posted successfully' })
+  } catch (error) {
+    console.log(error)
+    res
+      .status(500)
+      .json({ message: 'Internal error occured, try after some time' })
+  }
+})
 
 router.get('/', async (req, res) => {
-	try {
-		const posts = await Blog.find()
-			.select('-content -writer')
-			.sort({ createdAt: -1 });
+  try {
+    const posts = await Blog.find()
+      .select('-content -writer')
+      .sort({ createdAt: -1 })
 
-		res.status(200).json({ posts });
-	} catch (error) {
-		res.status(500).json({ message: 'some internal error occured' });
-	}
-});
+    res.status(200).json({ posts })
+  } catch (error) {
+    res.status(500).json({ message: 'some internal error occured' })
+  }
+})
 
 router.get('/trending', async (req, res) => {
-	try {
-		const posts = await Blog.find()
-			.select('-content -writer')
-			.sort({ views: -1 });
+  try {
+    const posts = await Blog.find()
+      .select('-content -writer')
+      .sort({ views: -1 })
 
-		res.status(200).json({ posts });
-	} catch (error) {
-		res.status(500).json({ message: 'some internal error occured' });
-	}
-});
+    res.status(200).json({ posts })
+  } catch (error) {
+    res.status(500).json({ message: 'some internal error occured' })
+  }
+})
 
 router.get('/post/:id', async (req, res) => {
-	try {
-		const { id } = req.params;
-		const post = await Blog.findOne({ _id: id });
+  try {
+    const { id } = req.params
+    const post = await Blog.findOne({ _id: id })
 
-		res.status(200).json({ post });
+    res.status(200).json({ post })
 
-		post.views++;
-		await post.save();
-	} catch (error) {
-		console.log(error);
-		res.status(500).json({ message: 'some internal error occured!' });
-	}
-});
+    post.views++
+    await post.save()
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ message: 'some internal error occured!' })
+  }
+})
 
 //route for handling likes
 router.get('/like/:id', async (req, res) => {
-	try {
-		let userId;
-		if (req.headers['authorization']) {
-			const token = req.headers['authorization'].split(' ')[1];
-			userId = jwt.verify(token, 'test').userId;
-		}
+  try {
+    let userId
+    if (req.headers['authorization']) {
+      const token = req.headers['authorization'].split(' ')[1]
+      userId = jwt.verify(token, 'test').userId
+    }
 
-		const { id } = req.params;
+    const { id } = req.params
 
-		const likes = await Likes.findOne({ postId: id });
-		let liked = false;
+    const likes = await Likes.findOne({ postId: id })
+    let liked = false
 
-		const likedArray = likes.users.map((id) => JSON.stringify(id));
+    const likedArray = likes.users.map((id) => JSON.stringify(id))
 
-		if (userId) {
-			const found = likedArray.find((user) => user == JSON.stringify(userId));
-			if (found) {
-				liked = true;
-			}
-		}
+    if (userId) {
+      const found = likedArray.find((user) => user == JSON.stringify(userId))
+      if (found) {
+        liked = true
+      }
+    }
 
-		res.status(200).json({ likeCount: likes.users.length, liked: liked });
-	} catch (error) {
-		console.log(error);
-		res.status(500).json({ message: 'some internal error occured!' });
-	}
-});
+    res.status(200).json({ likeCount: likes.users.length, liked: liked })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ message: 'some internal error occured!' })
+  }
+})
 
 router.post('/like/:id', verifyUser, async (req, res) => {
-	try {
-		const { id } = req.params;
-		const userId = req.userId;
-		const likes = await Likes.findOne({ postId: id });
+  try {
+    const { id } = req.params
+    const userId = req.userId
+    const likes = await Likes.findOne({ postId: id })
 
-		let liked = false;
-		const likedArray = likes.users.map((id) => JSON.stringify(id));
+    let liked = false
+    const likedArray = likes.users.map((id) => JSON.stringify(id))
 
-		const found = likedArray.find((user) => user == JSON.stringify(userId));
+    const found = likedArray.find((user) => user == JSON.stringify(userId))
 
-		if (found) {
-			liked = false;
-			likes.users.pull(new ObjectId(userId));
-			console.log(likes);
-		} else {
-			liked = true;
-			likes.users.push(userId);
-		}
+    if (found) {
+      liked = false
+      likes.users.pull(new ObjectId(userId))
+      console.log(likes)
+    } else {
+      liked = true
+      likes.users.push(userId)
+    }
 
-		console.log(liked);
+    console.log(liked)
 
-		await likes.save();
-		res.status(200).json({ likeCount: likes.users.length, liked: liked });
-	} catch (error) {
-		console.log(error);
-		res.status(500).json({ message: 'some internal error occured!' });
-	}
-});
+    await likes.save()
+    res.status(200).json({ likeCount: likes.users.length, liked: liked })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ message: 'some internal error occured!' })
+  }
+})
 
 // Handling comments in the route
 router.get('/comment/:id', verifyUser, async (req, res) => {
-	try {
-		const { id } = req.params;
-		const comments = await Comments.findOne({ postId: id });
-		console.log(comments.commentList);
-		res.status(200).json({ commentList: comments.commentList });
-	} catch (error) {
-		res.status(500).json({ message: 'some internal error occured!' });
-	}
-});
+  try {
+    const { id } = req.params
+    const comments = await Comments.findOne({ postId: id })
+    console.log(comments.commentList)
+    res.status(200).json({ commentList: comments.commentList })
+  } catch (error) {
+    res.status(500).json({ message: 'some internal error occured!' })
+  }
+})
 
 router.post('/comment/:id', verifyUser, async (req, res) => {
-	try {
-		const { text } = req.body;
-		const userId = req.userId;
-		const { id } = req.params;
+  try {
+    const { text } = req.body
+    const userId = req.userId
+    const { id } = req.params
 
-		let comments = await Comments.findOne({ postId: id });
-		console.log(comments);
-		if (!comments) {
-			comments = new Comments({
-				postId: id,
-				commentList: [
-					{
-						text: text,
-						user: userId,
-					},
-				],
-			});
-		} else {
-			const comment = {
-				text: text,
-				user: userId,
-			};
-			comments.commentList.push(comment);
-		}
+    let comments = await Comments.findOne({ postId: id })
+    console.log(comments)
+    if (!comments) {
+      comments = new Comments({
+        postId: id,
+        commentList: [
+          {
+            text: text,
+            user: userId,
+          },
+        ],
+      })
+    } else {
+      const comment = {
+        text: text,
+        user: userId,
+      }
+      comments.commentList.push(comment)
+    }
 
-		await comments.save();
-		res.status(201).json({ commentList: comments.commentList });
-	} catch (error) {
-		console.log(error);
-		res.status(500).json({ message: 'some internal error occured!' });
-	}
-});
+    await comments.save()
+    res.status(201).json({ commentList: comments.commentList })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ message: 'some internal error occured!' })
+  }
+})
 
-module.exports = router;
+module.exports = router
