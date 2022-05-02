854862af72c570d23afb97c667dddedd00c6ef66
rohit pandit <rohit0311@outlook.com>
2022-05-02 09:54:26 +0530

    Revert "testing"

--- server.js
+++ server.js
@@ -1,30 +1,29 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const morgan = require('morgan')
-require('./db')
-const auth = require('./routes/auth')
-const blog = require('./routes/blog')
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const morgan = require('morgan');
+require('./db');
+const auth = require('./routes/auth');
+const blog = require('./routes/blog');
 
-app.use(cors())
-app.use(morgan('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello world' })
-})
+	res.json({ message: 'Hello world' });
+});
 
-app.use('/auth', auth)
-app.use('/blog', blog)
+app.use('/auth', auth);
+app.use('/blog', blog);
 
 app.get('*', (req, res) => {
-  console.log('got request for page not found!')
-  res.status(404).send('Page not found')
-})
+	res.status(404).send('Page not found');
+});
 
 app.listen(PORT, () => {
-  console.log('server live at port:', PORT)
-})
+	console.log('server live at port:', PORT);
+});
